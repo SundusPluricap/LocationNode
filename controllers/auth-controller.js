da513fa72f110b625910c16e271043140406c710
SundusPluricap <sundus.pluricap@gmail.com>
2023-07-18 14:44:17 +0200

    add login and register

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -3,5 +3,14 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 export const index = (req, res) => {
-    res.render('home/index', { posts: [] });
-  };
+  res.render('home/index', { posts: [] });
+};
+
+export const login = (req, res) => {
+  res.render('home/login', { posts: [] });
+};
+
+
+export const register = (req, res) => {
+  res.render('home/register', { posts: [] });
+};
--- routes/auth_route.js
+++ routes/auth_route.js
@@ -1,11 +1,13 @@
 import express from "express";
-import {index} from "../controllers/auth-controller.js";
+import {index,login,register} from "../controllers/auth-controller.js";
 // import {existUser, authenticateUser} from "../middlewares/auth.js"
 
 const router = express.Router();
 
 
 router.get('/', index);
+router.get("/login", login);
+router.get("/register", register);
 
 // Root route
 // router.get("/", index);
